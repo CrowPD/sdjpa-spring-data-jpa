fd3dba64445f3a71df07ca1ad4ac8bc79a0eb66c
CrowPD <anubis.dead.god@gmail.com>
2024-08-22 21:38:13 +0300

    Import optimized

--- src/test/java/guru/springframework/jdbc/DaoIntegrationTest.java
+++ src/test/java/guru/springframework/jdbc/DaoIntegrationTest.java
@@ -9,11 +9,9 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Created by jt on 8/28/21.
